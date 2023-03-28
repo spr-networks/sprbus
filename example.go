cd83738cbc0e761ddb3b20af07ce813bc55f7051
lts-po <po@longterm.io>
2023-03-28 18:04:00 +0200

    rename example code

---
+++ example.go
@@ -0,0 +1,101 @@
+package sprbus
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"sync"
+	"time"
+)
+
+var client *Client
+
+func spr_cli() {
+
+	time.Sleep(time.Second / 4)
+
+	client, err := NewClient("")
+	defer client.Close()
+
+	if err != nil {
+		log.Fatal("err", err)
+	}
+
+	var wg sync.WaitGroup
+
+	fmt.Println("got client:", client)
+
+	stream, err := client.SubscribeTopic("spr:test")
+	if nil != err {
+		log.Fatal(err)
+	}
+
+	go func() {
+		fmt.Println("recv")
+		wg.Add(1)
+		for {
+			reply, err := stream.Recv()
+			if io.EOF == err {
+				break
+			}
+
+			if nil != err {
+				log.Fatal("ERRRRRR ", err) // Cancelled desc
+			}
+
+			value := reply.GetValue()
+
+			fmt.Printf("sub:reply: %v\n", reply)
+			fmt.Println("sub:value:", value)
+			//wg.Done()
+			//return
+		}
+	}()
+
+	for i := 1; i < 2; i++ {
+
+		time.Sleep(time.Second / 4)
+
+		fmt.Println("pub")
+		_, err = client.Publish("spr:test", "samplemsg")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = client.Publish("rpc:test", "samplemsg")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+
+	//wg.Wait()
+
+	fmt.Println("done")
+
+}
+
+func spr_server() {
+
+	fmt.Println("server listening...")
+
+	server, err := NewServer("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// does not return
+
+	fmt.Println("server:", server)
+}
+
+func main() {
+	fmt.Println("main")
+
+	go spr_server()
+	spr_cli()
+
+	for i := 1; i < 10; i++ {
+		time.Sleep(time.Second)
+
+	}
+}
