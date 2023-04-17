318ddd0f33b6b67caef09a06c852b6e2046ec5ab
lts-po <po@longterm.io>
2023-04-18 00:28:48 +0200

    [sprbus] add ws client, cli to view and publish events

--- cmd/go.mod
+++ cmd/go.mod
@@ -1,24 +1,49 @@
-module example
+module main
 
 go 1.20
 
-replace github.com/spr-networks/sprbus v0.1.4 => ../
-
-require github.com/spr-networks/sprbus v0.1.4
+require (
+	github.com/charmbracelet/bubbles v0.15.0
+	github.com/charmbracelet/bubbletea v0.23.2
+	github.com/charmbracelet/glamour v0.6.0
+	github.com/charmbracelet/lipgloss v0.7.1
+	github.com/gorilla/websocket v1.5.0
+	github.com/spr-networks/sprbus v0.1.7
+	github.com/tidwall/gjson v1.14.4
+)
 
 require (
-	github.com/fatih/color v1.15.0 // indirect
+	github.com/alecthomas/chroma v0.10.0 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/dlclark/regexp2 v1.4.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.21 // indirect
 	github.com/moby/moby v23.0.1+incompatible // indirect
 	github.com/moby/pubsub v1.0.0 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.1 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
-	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/yuin/goldmark v1.5.2 // indirect
+	github.com/yuin/goldmark-emoji v1.0.1 // indirect
 	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/genproto v0.0.0-20230227214838-9b19f0bdc514 // indirect
 	google.golang.org/grpc v1.53.0 // indirect
--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,18 @@
 package main
 
+//⛺︎⛺︎⛺︎⛺︎
 // this is a script to view the sprbus
+
+/*
+TODO
+
+* pager subview when selecting items
+    * json / key/value colorized output
+
+m.Index() (list.Model.Index()) in delegate.go
+how to share models? delegates vs. list
+
+*/
 
 import (
 	b64 "encoding/base64"
@@ -8,24 +20,31 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"github.com/fatih/color"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/spr-networks/sprbus"
 	"github.com/tidwall/gjson"
-	//"log"
+	"log"
 	"os"
 	"strings"
 	"syscall"
 	"time"
 )
 
-var socket = "/state/api/eventbus.sock"
+var (
+	socket   = "/state/api/eventbus.sock"
+	fgYellow = lipgloss.NewStyle().Foreground(lipgloss.Color("3")).Render
+	fgCyan   = lipgloss.NewStyle().Foreground(lipgloss.Color("6")).Render
+)
 
-func checkAccess(fileName string) error {
+func checkAccess(fileName string, checkWrite bool) error {
 	fileInfo, err := os.Stat(fileName)
 	if err != nil {
-		fmt.Printf("Error accessing file %s: %v\n", fileName, err)
+		log.Fatal(err)
 		return err
 	}
+
 	mode := fileInfo.Mode()
 
 	stat, ok := fileInfo.Sys().(*syscall.Stat_t)
@@ -38,7 +57,8 @@ func checkAccess(fileName string) error {
 	euid := os.Geteuid()
 	egid := os.Getegid()
 
-	if uid != euid && gid != egid && string(mode) != "Srwxrwxrwx" {
+	if uid != euid && gid != egid && string(mode.String()[7]) != "r" ||
+		(checkWrite && string(mode.String()[8]) != "w") {
 		return errors.New(fmt.Sprintf("bad file permissions on %v (%v)\n"+
 			"\tchmod a+rw or run with sudo", fileName, mode))
 	}
@@ -56,70 +76,142 @@ func StartsWithAny(t string, list []string) bool {
 	return false
 }
 
-func sub(filter string, exclude string, dumpJSON bool) {
-	go sprbus.HandleEvent("", func(topic string, json string) {
-		ct := color.New(color.FgCyan).SprintFunc()
+func ParseDesc(topic string, json string) string {
+	gjsons := map[string]string{
+		"www:log:access": "@values",
+		"devices:save":   "@keys",
+	}
 
-		if exclude != "" && strings.HasPrefix(topic, exclude) {
-			return
-		}
+	gjsonFilter := ""
+	if gjsons[topic] != "" {
+		gjsonFilter = gjsons[topic]
+	}
 
-		if filter != "" && !StartsWithAny(topic, strings.Split(filter, ",")) {
-			return
-		}
-
-		if dumpJSON {
-			fmt.Println(json)
-			return
-		}
-
-		gjsons := map[string]string{
-			"www:log:access": "@values",
-			"devices:save":   "@keys",
-		}
-
-		gjsonFilter := ""
-		if gjsons[topic] != "" {
-			gjsonFilter = gjsons[topic]
-		}
-
-		if topic == "www:log:access" {
-			fmt.Printf("%12v %v\n", ct(topic), gjson.Get(json, gjsonFilter))
-		} else if topic == "devices:save" {
-			fmt.Printf("%12s %v\n", ct(topic), gjson.Get(json, gjsonFilter))
-		} else if strings.HasPrefix(topic, "device:") {
-			fmt.Printf("%12s %v\n", ct(topic), gjson.Get(json, "MAC"))
-		} else if strings.HasPrefix(topic, "wifi") {
-			fmt.Printf("%12v %v\n", ct(topic), gjson.Get(json, "@values"))
-		} else if strings.HasPrefix(topic, "nft") {
-			if gjson.Get(json, "DNS.Questions").Exists() {
-				q := gjson.Get(json, "DNS.Questions.0.Name")
-				name, _ := b64.StdEncoding.DecodeString(q.String())
-				fmt.Printf("%v %v lookup %v\n", ct(topic),
-					gjson.Get(json, "IP.SrcIP"), string(name))
-			} else if gjson.Get(json, "UDP.SrcPort").Exists() {
-				fmt.Printf("%v %v:%v -> %v:%v\n", ct(topic),
-					gjson.Get(json, "IP.SrcIP"), gjson.Get(json, "UDP.SrcPort"),
-					gjson.Get(json, "IP.DstIP"), gjson.Get(json, "UDP.DstPort"))
-			} else {
-				fmt.Printf("%v %v:%v -> %v:%v\n", ct(topic),
-					gjson.Get(json, "IP.SrcIP"), gjson.Get(json, "TCP.SrcPort"),
-					gjson.Get(json, "IP.DstIP"), gjson.Get(json, "TCP.DstPort"))
-			}
+	desc := ""
+	if topic == "www:log:access" {
+		desc = gjson.Get(json, gjsonFilter).String()
+	} else if topic == "devices:save" {
+		desc = gjson.Get(json, gjsonFilter).String()
+	} else if strings.HasPrefix(topic, "device:") {
+		desc = gjson.Get(json, "MAC").String()
+	} else if strings.HasPrefix(topic, "wifi") {
+		desc = gjson.Get(json, "@values").String()
+	} else if topic == "dns:block:event" {
+		desc = fmt.Sprintf("%v %v", gjson.Get(json, "ClientIP").String(),
+			gjson.Get(json, "Name"))
+	} else if topic == "dns:serve:event" {
+		desc = fmt.Sprintf("%v %v", gjson.Get(json, "Type").String(),
+			gjson.Get(json, "Q.0.Name"))
+	} else if strings.HasPrefix(topic, "nft") {
+		if gjson.Get(json, "DNS.Questions").Exists() {
+			q := gjson.Get(json, "DNS.Questions.0.Name")
+			name, _ := b64.StdEncoding.DecodeString(q.String())
+			desc = fmt.Sprintf("%v lookup %v",
+				gjson.Get(json, "IP.SrcIP"), string(name))
+		} else if gjson.Get(json, "UDP.SrcPort").Exists() {
+			desc = fmt.Sprintf("%v:%v -> %v:%v\n",
+				gjson.Get(json, "IP.SrcIP"), gjson.Get(json, "UDP.SrcPort"),
+				gjson.Get(json, "IP.DstIP"), gjson.Get(json, "UDP.DstPort"))
 		} else {
-			fmt.Printf("%12v %v\n", ct(topic), json)
+			desc = fmt.Sprintf("%v:%v -> %v:%v",
+				gjson.Get(json, "IP.SrcIP"), gjson.Get(json, "TCP.SrcPort"),
+				gjson.Get(json, "IP.DstIP"), gjson.Get(json, "TCP.DstPort"))
 		}
+	} else {
+		desc = gjson.Get(json, "@values").String()
+	}
+
+	return desc
+}
+
+// TODO make same as filterSub
+func handleData(topic string, json string, filter string, exclude string, callback func(string, string)) {
+	if exclude != "" && strings.HasPrefix(topic, exclude) {
+		return
+	}
+
+	if filter != "" && !StartsWithAny(topic, strings.Split(filter, ",")) {
+		return
+	}
+
+	callback(topic, json)
+}
+
+func filterSub(filter string, exclude string, callback func(string, string)) {
+	go sprbus.HandleEvent("", func(topic string, json string) {
+		handleData(topic, json, filter, exclude, callback)
 	})
+}
+
+func filterWS(addr string, filter string, exclude string, callback func(string, string)) {
+	authString := os.Getenv("TOKEN")
+	if authString == "" {
+		log.Fatal("missing TOKEN in environment")
+	}
+
+	go ConnectWebsocket(addr, authString, func(topic string, json string) {
+		handleData(topic, json, filter, exclude, callback)
+	})
+}
+
+// json representation of sprbus logs
+type BusLogItem struct {
+	Topic string
+	Value map[string]interface{}
+}
+
+func preloadItems(fileName string) []list.Item {
+	maxNum := 1
+
+	var data []byte
+	var err error
+
+	if data, err = os.ReadFile(fileName); err != nil {
+		log.Fatal(err)
+	}
+
+	items := []list.Item{}
+
+	result := gjson.Parse(string(data))
+	for i, event := range result.Array() {
+		title := gjson.Get(event.String(), "Topic").String()
+		json := gjson.Get(event.String(), "Value").String()
+		description := ParseDesc(title, json)
+
+		v := item{title: title, description: description, json: json}
+		items = append(items, v)
+
+		if i >= maxNum {
+			break
+		}
+	}
+
+	return items
+}
+
+func initGUI() *tea.Program {
+	items := []list.Item{} //TODO preloadItems() if -filename
+
+	m := NewModel(items)
+	p := tea.NewProgram(m)
+
+	return p
 }
 
 func main() {
 	help := flag.Bool("help", false, "show help")
+	addr := flag.String("addr", "", "http service address, example: 192.168.2.1:80")
 	dumpJSON := flag.Bool("j", false, "show json output")
-	topic := flag.String("t", "", "topic")
-	exclude := flag.String("e", "", "exclude")
+	topic := flag.String("t", "", "topic(s) to filter. Example www:log,wifi:auth")
+	exclude := flag.String("e", "nft:lan:in", "exclude topic(s). Example nft:lan:in,nft:wan:out")
 	publish := flag.String("p", "", "publish, raw json data")
 	timeout := flag.Int("timeout", 20, "exit timeout")
 	verbose := flag.Bool("v", false, "verbose output")
+	noGUI := flag.Bool("nogui", false, "dont show gui, print to stdout")
+
+	//TODO add these
+	//fileNameIn := flag.String("f", "", "read sprbus json data from file")
+	//fileNameOut := flag.String("o", "", "write sprbus json data to file")
 
 	flag.Parse()
 
@@ -128,9 +220,16 @@ func main() {
 		os.Exit(0)
 	}
 
-	if err := checkAccess(socket); err != nil {
-		ct := color.New(color.FgYellow).SprintFunc()
-		fmt.Println(ct("NOTE"), err)
+	isRemote := *addr != ""
+
+	if *dumpJSON {
+		*noGUI = true
+	}
+
+	if !isRemote {
+		if err := checkAccess(socket, *publish != ""); err != nil {
+			fmt.Println(fgYellow("NOTE"), err)
+		}
 	}
 
 	if *publish != "" && *topic != "" {
@@ -144,10 +243,57 @@ func main() {
 			}
 		}
 
+		if isRemote {
+			log.Println("TODO not implemented")
+			return
+		}
+
 		sprbus.PublishString(*topic, *publish)
 		return
+	}
+
+	var p *tea.Program
+
+	if !*noGUI {
+		p = initGUI()
+	}
+
+	// we get same data from either sprbus or websocket here
+	retSub := func(title string, json string) {
+		description := ParseDesc(title, json)
+
+		//send to view
+		if *noGUI {
+			if *dumpJSON {
+				fmt.Printf("{\"Topic\": \"%s\", \"Value\": %s}\n", title, json)
+
+				return
+			}
+
+			//else just print to stdout
+			fmt.Printf("%v %v\n", fgCyan(title), description)
+
+			return
+		}
+
+		p.Send(EventMsg{title: title, description: description, json: json})
+
+		return
+	}
+
+	if isRemote {
+		filterWS(*addr, *topic, *exclude, retSub)
 	} else {
-		sub(*topic, *exclude, *dumpJSON)
+		filterSub(*topic, *exclude, retSub)
+	}
+
+	if !*noGUI {
+		if _, err := p.Run(); err != nil {
+			fmt.Println("Error running program:", err)
+			os.Exit(1)
+		}
+
+		return
 	}
 
 	if *timeout != 0 {
